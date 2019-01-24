976bdc5f9281edce0a9cef4eb4a0286f55507558
Andrey Voronin <andrey.voronin@cd-adapco.com>
2019-01-24 09:36:57 +0300

    update

--- vset.sh
+++ vset.sh
@@ -1,4 +1,4 @@
-export PATH=/home/voran/vbuild/workspace/bin:$PATH
-export LD_LIBRARY_PATH=/home/voran/vbuild/workspace/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/home/voran/vbuild/workspace/lib:$PYTHONPATH
-export PYTHONPATH=/home/voran/vbuild/workspace/lib/python3.6/site-packages:$PYTHONPATH
+export PATH=workspace/bin:$PATH
+export LD_LIBRARY_PATH=workspace/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=workspace/lib:$PYTHONPATH
+export PYTHONPATH=workspace/lib/python3.6/site-packages:$PYTHONPATH
