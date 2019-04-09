0d5d7c8ce8f14afbc63d31ff00b774aa6406f768
voran <voran@inbox.ru>
2019-04-09 15:03:09 +0300

    svp test has been added

---
+++ test/convert-to-60fps.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! -f "../workspace/lib/vapoursynth/libsvpflow1_vs64.so" ] && [ ! -f "../workspace/lib/vapoursynth/libsvpflow2_vs64.so" ]; then
+	mkdir /tmp/svp
+
+	curl --silent http://www.svp-team.com/files/gpl/svpflow-4.2.0.142.zip --output /tmp/svp/plugin.zip
+
+	unzip -o -d /tmp/svp /tmp/svp/plugin.zip
+
+	cp /tmp/svp/svpflow-*/lib-linux/*.so ../workspace/lib/vapoursynth/ 
+
+	rm -rf /tmp/svp
+
+else
+
+	echo "SVP libraries were already downloaded"
+
+fi
+
+vspipe --y4m convert-to-60fps.pvy - | ffplay -i pipe:
+
--- vset.sh
+++ vset.sh
@@ -1,4 +1,4 @@
-export PATH=/home/linuxuser/vbuild/workspace/bin:$PATH
-export LD_LIBRARY_PATH=/home/linuxuser/vbuild/workspace/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/home/linuxuser/vbuild/workspace/lib:$PYTHONPATH
-export PYTHONPATH=/home/linuxuser/vbuild/workspace/lib/python3.6/site-packages:$PYTHONPATH
+export PATH=$PWD/workspace/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/workspace/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$PWD/workspace/lib:$PYTHONPATH
+export PYTHONPATH=$PWD/workspace/lib/python3.6/site-packages:$PYTHONPATH
