705eb50f6f16a836de148730ceb2bae9716f7b58
voran <voran@inbox.ru>
2020-01-06 20:54:56 +0300

    update

--- tests/fpska/60fps/60fps.sh
+++ tests/fpska/60fps/60fps.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
 
-#if [ ! -f "../../workspace/lib/vapoursynth/libsvpflow1_vs64.so" ] && [ ! -f "../../workspace/lib/vapoursynth/libsvpflow2_vs64.so" ]; then
-#	mkdir /tmp/svp
-
-#	curl --silent https://www.svp-team.com/files/gpl/svpflow-4.3.0.168.zip --output /tmp/svp/plugin.zip
-
-#	unzip -o -d /tmp/svp /tmp/svp/plugin.zip
-
-#	cp /tmp/svp/svpflow-*/lib-linux/*.so ../../workspace/lib/vapoursynth/
-
-#	rm -rf /tmp/svp
-
-#else
-
-#	echo "SVP libraries were already downloaded"
-
-#fi
-
-vspipe --y4m 60fps.pvy - | ffmpeg -i pipe: -c:a copy -c:v libx264 -crf 20 -preset slow ../tmp/60fps.mp4
-#vspipe --y4m 60fps.pvy - | ffplay -i pipe:
+vspipe --y4m 60fps.pvy - | ffmpeg -hide_banner -loglevel panic -i pipe: -c:a copy -c:v libx264 -crf 20 -preset slow ../tmp/60fps.mp4
--- tests/fpska/fpska.sh
+++ tests/fpska/fpska.sh
@@ -1,12 +1,94 @@
 CWD=$(pwd)
 
+
+# --------------------------------------------------------------------------------
+
+demuxer() {
+
+video=$1
+
+echo $video
+
+STREAMS=$(ffprobe -i $video 2>&1 | egrep Stream)
+
+demuxer="ffmpeg -hide_banner -loglevel panic -i $1 -y " 
+
+while IFS= read -r line; do
+
+	map=$(echo $line | egrep -o 'Stream #[0-9]+:[0-9]+' | sed -r 's/Stream \#//')
+	snumber=$(echo $map | sed -r 's/[0-9]\://')
+
+case $line in
+     *"h264"*)
+          demuxer+="-map $map -c copy tmp/video-stream-$snumber.h264 "
+          ;;
+     *"mpeg4"*)
+          demuxer+="-map $map -c copy tmp/video-stream-$snumber.mp4 "
+          ;;
+     *"aac"*)
+          demuxer+="-map $map -c copy tmp/audio-stream-$snumber.aac "
+          ;;
+     *"ac3"*)
+          demuxer+="-map $map -c copy tmp/audio-stream-$snumber.ac3 "
+          ;;
+     *"wmv2"*)
+          demuxer+="-map $map -c copy tmp/video-stream-$snumber.wmv "
+          ;;
+     *"wmav2"*)
+          demuxer+="-map $map -c copy tmp/audio-stream-$snumber.wma "
+          ;;
+     *"mp3"*)
+          demuxer+="-map $map -c copy tmp/audio-stream-$snumber.mp3 "
+          ;;
+     *"dts"*)
+          demuxer+="-map $map -c copy tmp/audio-stream-$snumber.dts "
+          ;;
+     *"pcm"*)
+          demuxer+="-map $map -c pcm_s24le tmp/audio-stream-$snumber.wav "
+          ;;
+     *"Subtitle"*)
+          demuxer+="-map $map tmp/subtitle-stream-$snumber.srt "
+          ;;
+     *)
+          ;;
+esac
+
+done <<< "$STREAMS"
+
+echo "$demuxer"
+
+$demuxer
+
+}
+
+muxer() {
+
+filename=$(basename -- $1)
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+files=$( ls tmp/* )
+
+counter=0
+mkv="mkvmerge "
+for i in $files ; do
+  mkv+="$i "
+done
+mkv+="-o $filename-60fps.mkv"
+
+$mkv
+
+}
+
+
+
 rm -rf tmp
 mkdir tmp
 
-cd demux
-./demux.sh ${CWD}/$1
+echo ${CWD}/$1
+demuxer ${CWD}/$1
+echo ---------------------------------------------------------------------------
 
-cd ../
 video_stream=$(find tmp -name "video*")
 echo ${CWD}/$video_stream
 
@@ -14,10 +96,12 @@ cp ${CWD}/60fps/60fps.template ${CWD}/60fps/60fps.pvy
 
 sed -i "s|\$VIDEO|${CWD}\/$video_stream|g" ${CWD}/60fps/60fps.pvy
 
-cd ${CWD}/60fps
-pwd
-./60fps.sh
+#cd ${CWD}/60fps
+#pwd
+60fps/60fps.sh
 
-cd ${CWD}/tmp
-rm -f video-stream*
+#cd ${CWD}/tmp
+rm -f tmp/video-stream*
+
+muxer ${CWD}/$1
 
